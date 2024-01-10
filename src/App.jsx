dd39435bc0f1c03085e7fa7a4e0048900d57863b
Fleuron Gnadekpa <vilfleur@gmail.com>
2024-01-10 03:26:08 +0000

    update name, about and socials

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Fleuron Gnadekpa",
+  title: "Part-time Frontend Developer.",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  gitHub: "fleuronvilik",
+  instagram: "gdk_fleuron",
+  linkedIn: "fleurongnadekpa",
   medium: "",
-  twitter: "microsoft",
+  twitter: "FGnadekpa",
   youTube: "Code",
 };
 
